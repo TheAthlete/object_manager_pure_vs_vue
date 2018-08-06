ded8941e4a032fa906f4c3ab039b61346c1cf964
TheAthlete <theathlet@yandex.ru>
2018-08-06 17:21:27 +0300

    Initial commit

---
+++ object_manager_vue/src/components/ObjectManagerVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="object_manager">
+
+    <yandex-map 
+      :coords="[55.76, 37.64]"
+      style="width: 600px; height: 600px;"
+      zoom="10"
+      :use-object-manager="true"
+    >
+    </yandex-map>
+  </div>
+</template>
+
+<script>
+import { yandexMap, ymapMarker } from 'vue-yandex-maps'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    yandexMap, 
+    ymapMarker
+  }
+}
+</script>
